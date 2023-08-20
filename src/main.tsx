1c81134aa741b9d856d7bee5b43e78f9805d13d5
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2023-08-20 08:55:11 -0700

    Add QuioscoProvider and RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+import { QuioscoProvider } from './context/QuioscoProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QuioscoProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </QuioscoProvider>
+  </React.StrictMode>
+);
