716048bfdf3fd9348ad0574b2b262d69e0460698
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2023-11-03 17:29:02 +0100

    Home Screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,35 @@
 import Produto from '../components/Produto';
-import { productos as data } from '../data/produtos';
 import useQuiosco from '../hooks/useQuiosco';
+import useSWR from 'swr';
+import apiClient from '../services/api-client';
+import { ProdutoProps } from '../data/produtos';
+
+export interface ProductResponse {
+  data: ProdutoProps[];
+}
 
 export default function Home() {
   const { categoriaActual } = useQuiosco();
+  const token = localStorage.getItem('AUTH_TOKEN');
 
-  const products = data.filter(
-    (product) => product.category_id === categoriaActual.id
+  const fetcher = () =>
+    apiClient.get<ProductResponse>('/api/products', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then((data) => {
+      return data.data;
+    });
+  const { data, isLoading } = useSWR<ProductResponse>(
+    '/api/products',
+    fetcher, {
+      refreshInterval: 5000,
+    }
   );
+  if (isLoading) return 'Loading';
+  const products =
+    data?.data &&
+    data.data.filter((product) => product.categoria_id === categoriaActual.id);
   return (
     <>
       <h1
@@ -16,14 +38,15 @@ export default function Home() {
       >
         {categoriaActual.name}
       </h1>
-      <p>Elege e personalizao teu pedido</p>
+      <p>Elege e personaliza o teu pedido</p>
       <div
         className="grid gap-4 grid-cols-1
-    md:grid-cols-2 xl:grid-cols-3"
+    md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"
       >
-        {products.map((produto) => (
-          <Produto key={produto.image} produto={produto} />
-        ))}
+        {products &&
+          products.map((produto) => (
+            <Produto key={produto.id} produto={produto} addButton={true} />
+          ))}
       </div>
     </>
   );
