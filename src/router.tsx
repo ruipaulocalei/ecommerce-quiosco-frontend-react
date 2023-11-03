8cd43bf986d58ade8d40b682b28112396594c65d
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2023-11-03 17:23:28 +0100

    Adding routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,34 +1,53 @@
-import { createBrowserRouter } from "react-router-dom";
-import Layout from "./layouts/Layout";
-import AuthLayout from "./layouts/AuthLayout";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from './layouts/Layout';
+import AuthLayout from './layouts/AuthLayout';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import AdminLayout from './layouts/AdminLayout';
+import Orders from './pages/Orders';
+import Products from './pages/Products';
 
-const router = createBrowserRouter([{
+const router = createBrowserRouter([
+  {
     path: '/',
     element: <Layout />,
-    children:[
-        {
-            index: true,
-            element: <Home />,
-        }
-    ]
-}, 
-{
+    children: [
+      {
+        
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+  {
     path: '/auth',
     element: <AuthLayout />,
     children: [
+      {
+        path: '/auth/login',
+        element: <Login />,
+      },
+      {
+        path: '/auth/registro',
+        element: <Register />,
+      },
+    ],
+  },
+  {
+    path: '/admin',
+    element: <AdminLayout />,
+    children: [
         {
-            path: '/auth/login',
-            element: <Login />
+            index: true,
+            element: <Orders />
         },
         {
-            path: '/auth/registro',
-            element: <Register />
-        },
+            path: '/admin/products',
+            element: <Products />
+        }
     ]
-},
-])
+  },
+]);
 
-export default router
+export default router;
