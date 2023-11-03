a1d317f12d9de4264beb706c0d2a0c2833118697
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2023-11-03 17:25:06 +0100

    Product Screen

--- src/components/Produto.tsx
+++ src/components/Produto.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { ProdutoProps } from '../data/produtos';
 import { formatCurrency } from '../helpers';
+import useQuiosco from '../hooks/useQuiosco';
 
 interface Props {
   produto: ProdutoProps;
+  addButton?: boolean;
+  availableButton?: boolean;
 }
-export default function Produto({ produto }: Props) {
-  const { name, price, image } = produto;
+export default function Produto({
+  produto,
+  addButton,
+  availableButton,
+}: Props) {
+  const { handleClickModal, handleSetProduct, handleClickProductNotAvailable } =
+    useQuiosco();
+  const { id, name, price, image } = produto;
   return (
     <div className="border p-3 shadow bg-white">
       <img
@@ -20,14 +29,33 @@ export default function Produto({ produto }: Props) {
         <p className="mt-3 font-black text-4xl text-amber-500">
           {formatCurrency(price)}
         </p>
-        <button
-          type="button"
-          className="uppercase text-white 
+        {addButton && (
+          <button
+            type="button"
+            className="uppercase text-white 
           bg-indigo-600 hover:bg-indigo-800
         w-full mt-5 p-3 font-bold"
-        >
-          adicionar
-        </button>
+            onClick={() => {
+              handleClickModal(), handleSetProduct(produto);
+            }}
+          >
+            adicionar
+          </button>
+        )}
+
+        {availableButton && (
+          <button
+            type="button"
+            className="uppercase text-white 
+          bg-indigo-600 hover:bg-indigo-800
+        w-full mt-5 p-3 font-bold"
+            onClick={() => {
+              handleClickProductNotAvailable(id);
+            }}
+          >
+            Unavailable product
+          </button>
+        )}
       </div>
     </div>
   );
